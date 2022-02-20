fcee37907d47a9048e91049bd68c067d97102b13
Thomas Vickers <vickerst10@gmail.com>
2022-02-20 13:26:56 -0700

    Header for project added

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header () {
+    return (
+        <header>
+            <img src="./trollface.png" alt="logo" className="header--image" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">Thomas Vickers</h4>
+        </header>
+    )
+
+}
